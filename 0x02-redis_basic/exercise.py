05a59847c3f855b433981c78e8b8f045e1c818d0
Olayiwola Sulaimon <osulaimon19@yahoo.com>
2024-10-24 14:33:02 +0000

    Reading and recovering

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,7 @@
 
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Optional
 
 
 class Cache:
@@ -20,3 +20,36 @@ class Cache:
         self._redis.set(key, data)
 
         return key
+
+    def get(
+        self, key: str, fn: Optional[Callable] = None
+    ) -> Optional[Union[str, bytes, int, float]]:
+        """
+        Retrieve data from Redis and optionally apply a conversion function.
+
+        Args:
+            key (str): The key to retrieve data from Redis.
+            fn (Callable, optional): A function to convert the data to
+            a desired type.
+
+        Returns:
+            Optional[Union[str, bytes, int, float]]: The retrieved data or
+            None if the key does not exist.
+        """
+        data = self._redis.get(key)
+
+        if data is None:
+            return None
+
+        if fn:
+            return fn(data)
+
+        return data
+
+    def get_str(self, key: str) -> Optional[str]:
+        """Retrieve data from Redis and convert it to a UTF-8 string."""
+        return self.get(key, fn=lambda d: d.decode('utf-8'))
+
+    def get_int(self, key: str) -> Optional[int]:
+        """Retrieve data from Redis and convert it to an integer."""
+        return self.get(key, fn=int)
